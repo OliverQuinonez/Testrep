8bf9aada5c0f14ca1c1038cff8d4582f46339ec3
Oliver Quinonez <oliver.quinonez@colorado.edu>
2024-09-05 15:16:51 -0600

    five comments

--- Labs/Lab1/4.2.1.py
+++ Labs/Lab1/4.2.1.py
@@ -4,17 +4,17 @@ import numpy.linalg as la
 import math
 def driver():
     n = 100
-    x = np.linspace(-np.pi,np.pi,n)
+    x = np.linspace(-np.pi,np.pi,n)#defines interval from -pi to pi
     # this is a function handle. You can use it to define
     # functions instead of using a subroutine like you
     # have to in a true low level language.
     4
-    f = lambda x: np.sin(x)
-    g = lambda x: np.cos(x)
+    f = lambda x: np.sin(x) #implements function sin of x
+    g = lambda x: np.cos(x) #implements function cos of x
     y = f(x)
     w = g(x)
     # evaluate the dot product of y and w
-    dp = dotProduct(y,w,n)
+    dp = dotProduct(y,w,n) # should be equal to zero
     # print the output
     print('the dot product is : ', dp)
     return
@@ -23,7 +23,7 @@ def dotProduct(x,y,n):
     # Computes the dot product of the n x 1 vectors x and y
     dp = 0.
     for j in range(n):
-        dp = dp + x[j]*y[j] #comment 1
+        dp = dp + x[j]*y[j] #computes the drop product itterativley
         
     return dp
 
