ee0f36fe4449926290d240c57c01bbf2aa0200db
Oliver Quinonez <oliver.quinonez@colorado.edu>
2024-10-25 19:30:33 -0600

    added files from homework 8

---
+++ Homework/Homework 8/problem2a.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def driver():
+    f = lambda x: 1/(1+x**2)
+
+    N = 20
+    ''' interval'''
+    a = -5
+    b = 5
+   
+   
+    ''' create chebychev interpolation nodes'''
+    xdata = np.zeros(N+1)
+    for i in range(1,len(xdata)+1):
+       xdata[i-1] = 5*np.cos((2*i-1)*np.pi/(2*(N+1)))
+   
+    
+    ''' create interpolation data'''
+    ydata = f(xdata)
+    
+    ''' create points for evaluating the Lagrange interpolating polynomial'''
+    Neval = 1000
+    xeval = np.linspace(a,b,Neval+1)
+    yeval_l= np.zeros(Neval+1)
+
+    ''' evaluate lagrange poly '''
+    for kk in range(Neval+1):
+       yeval_l[kk] = eval_lagrange(xeval[kk],xdata,ydata,N)
+          
+    ''' create vector with exact values'''
+    fex = f(xeval)
+       
+
+    plt.figure()    
+    plt.plot(xeval,fex,'ro-')
+    plt.plot(xeval,yeval_l,'bs--') 
+    plt.title('Lagrange-Chebychev N=20')
+    plt.legend()
+
+    plt.figure() 
+    err_l = abs(yeval_l-fex)
+    plt.semilogy(xeval,err_l,'ro--',label='lagrange')
+    plt.legend()
+    plt.title('Lagrange-Chebychev Err N=20')
+    plt.show()
+
+
+
+
+
+
+
+def eval_lagrange(xeval,xint,yint,N):
+
+    lj = np.ones(N+1)
+    
+    for count in range(N+1):
+       for jj in range(N+1):
+           if (jj != count):
+              lj[count] = lj[count]*(xeval - xint[jj])/(xint[count]-xint[jj])
+
+    yeval = 0.
+    
+    for jj in range(N+1):
+       yeval = yeval + yint[jj]*lj[jj]
+  
+    return(yeval)
+
+driver()
